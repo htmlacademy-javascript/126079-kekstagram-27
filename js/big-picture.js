712632c4132f92b0d5afd4e28b8a34f3fcdecf99
Дарья <Nencytime.14.92@yandex.ru>
2022-11-10 00:06:44 +0300

    Объединяет модули отрисовки больших изображений и списка комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,42 +1,106 @@
-import {createCommentList} from './big-picture-comments.js';
-import {isEscapeKey} from './util.js';
+import {isEscapeKey, wordToNumber} from './util.js';
 
+const PART_OF_COMMENTS = 5;
+const ENDINGS = ['комментария', 'комментариев', 'комментариев'];
+
+const closeButton = document.querySelector('.big-picture__cancel');
 const bigPicture = document.querySelector('.big-picture');
-const imgage = bigPicture.querySelector('img');
-const likes = bigPicture.querySelector('.likes-count');
-const cancelPicture = bigPicture.querySelector('#picture-cancel');
-const socialCaption = bigPicture.querySelector('.social__caption');
-const body = document.querySelector('body');
-const cancelButton = document.querySelector('.big-picture__cancel');
 
-const hideBigPicture = () => {
+const bigPictureImage = document.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentItem = bigPicture.querySelector('.social__comment');
+
+const pictureDescription = bigPicture.querySelector('.social__caption');
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const socialCommentsLoader = bigPicture.querySelector('.social__comments-loader');
+
+let displayedComments = 0;
+let comments;
+let commentsLength;
+
+const closeModal = () => {
   bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onEscKeydown);
+  document.body.classList.remove('modal-open');
+  removeEventListener();
 };
 
-function onEscKeydown (evt) {
-  if (isEscapeKey) {
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
-    hideBigPicture();
+    closeModal();
   }
+};
+
+const onCloseButtonClick = (evt) => {
+  evt.preventDefault();
+  closeModal();
+};
+
+const renderComments = (array) => {
+  array.forEach((item) => {
+    const comment = commentItem.cloneNode(true);
+    const commentImg = comment.querySelector('.social__picture');
+    commentImg.src = item.avatar;
+    commentImg.alt = item.name;
+    comment.querySelector('.social__text').textContent = item.message;
+    commentsList.append(comment);
+  });
+};
+
+const updateCommentLoaderBtn = () => {
+  if (displayedComments === comments.length) {
+    socialCommentsLoader.classList.add('hidden');
+    return;
+  }
+  socialCommentsLoader.classList.remove('hidden');
+};
+
+const showComments = (from, to) => {
+  displayedComments = Math.min(to, comments.length);
+  renderComments(comments.slice(from, displayedComments));
+
+  socialCommentCount.textContent = `${displayedComments} из ${commentsLength} ${wordToNumber(commentsLength, ENDINGS)}`;
+  updateCommentLoaderBtn();
+};
+
+const onCommentLoaderBtnClick = (evt) => {
+  evt.preventDefault();
+  showComments(displayedComments, displayedComments + PART_OF_COMMENTS);
+};
+
+function removeEventListener() {
+  document.removeEventListener('keydown', onDocumentKeydown);
+  closeButton.removeEventListener('click', onCloseButtonClick);
+  socialCommentsLoader.removeEventListener('click', onCommentLoaderBtnClick);
 }
 
-const onCancelButtonClick = () => hideBigPicture();
+const addModalListeners = () => {
+  document.addEventListener('keydown', onDocumentKeydown);
+  closeButton.addEventListener('click', onCloseButtonClick);
+  socialCommentsLoader.addEventListener('click', onCommentLoaderBtnClick);
+};
 
-cancelButton.addEventListener('click', onCancelButtonClick);
+const fillBigPicture = (picture) => {
+  commentsList.innerHTML = '';
+  bigPictureImage.src = picture.url;
+  bigPictureImage.alt = picture.description;
+  likesCount.textContent = picture.likes;
+  pictureDescription.textContent = picture.description;
+};
 
-const showBigPicture = (imageData) => {
-
-  document.body.classList.add('modal-open');
-  cancelPicture.addEventListener('click', onCancelButtonClick);
-  window.addEventListener('keydown', onEscKeydown);
+const showBigPicture = (picture) => {
   bigPicture.classList.remove('hidden');
+  socialCommentCount.classList.remove('hidden');
+  document.body.classList.add('modal-open');
 
-  imgage.src = imageData.url;
-  likes.textContent = imageData.likes;
-  socialCaption.textContent = imageData.description;
-  createCommentList(imageData.comments);
+  comments = picture.comments;
+  commentsLength = comments.length;
+
+  fillBigPicture(picture);
+  showComments(0, PART_OF_COMMENTS);
+  addModalListeners();
 };
 
 export {showBigPicture};
