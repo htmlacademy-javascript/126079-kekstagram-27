cbcfaa96d554b80f00635dcf6f6b43734d4a46dd
Дарья <Nencytime.14.92@yandex.ru>
2022-11-13 22:00:54 +0300

    Исправляет баги

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
-import {isEscapeKey} from './util.js';
+import {isEscapeKey, showAlert} from './util.js';
 import {addValidator, pristineReset, pristineValidate} from './form-validate.js';
-import {resetFilter} from './picture-effects.js';
+import {activateScale, deactivateScale} from './scale-image.js';
+import {changeEffect, resetFilter} from './effects-image.js';
+import {sendData} from './api.js';
 
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
@@ -8,13 +10,18 @@ const cancelButton = document.querySelector('#upload-cancel');
 const fileField = document.querySelector('#upload-file');
 const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
+const effectsListElement = document.querySelector('.effects__list');
 const body = document.querySelector('body');
+
+const submitButton = document.querySelector('.img-upload__submit');
 
 const showModal = () => {
   overlay.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onEscKeydown);
   cancelButton.addEventListener('click', onCancelButtonClick);
+  effectsListElement.addEventListener('change', changeEffect);
+  activateScale();
 };
 
 const hideModal = () => {
@@ -25,6 +32,8 @@ const hideModal = () => {
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscKeydown);
   cancelButton.removeEventListener('click', onCancelButtonClick);
+  effectsListElement.removeEventListener('change', changeEffect);
+  deactivateScale();
 };
 
 function onCancelButtonClick () {
@@ -44,7 +53,47 @@ const onFileInputChange = () => {
   showModal();
 };
 
-const onSubmitForm = (evt) => {
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Загружаю...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const setUserFormSubmit = (onSuccess) => {
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (pristineValidate()) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+          unblockSubmitButton();
+        },
+        () => {
+          showAlert('Не удалось опубликовать изображение. Попробуйте ещё раз');
+          unblockSubmitButton();
+        },
+        new FormData(evt.target),
+      );
+    }
+  });
+};
+
+const addAddFormAction = () => {
+  addValidator();
+  fileField.addEventListener('change', onFileInputChange);
+};
+
+export {setUserFormSubmit, addAddFormAction};
+
+
+/* const onSubmitForm = (evt) => {
   if (!pristineValidate()) {
     evt.preventDefault();
   }
@@ -54,7 +103,5 @@ const addAddFormAction = () => {
   addValidator();
   fileField.addEventListener('change', onFileInputChange);
   form.addEventListener('submit', onSubmitForm);
-};
-
-export {addAddFormAction};
+}; */
 
